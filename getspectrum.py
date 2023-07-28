48df32b3cb1045afdc2e2f35f3ac38482b918089
sohryuu101 <sohryuuasuka@gmail.com>
2023-07-28 15:57:17 +0700

    \

---
+++ getspectrum.py
@@ -0,0 +1,98 @@
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+from scipy import signal
+# from scipy.fft import fftshift, fft2
+
+
+def readExcelData(excel: str, nSample=1024):
+    """
+    menerima masukan berupa nama file dan keluaran berupa 
+    numpy array 2D
+    
+    """
+    file = pd.read_excel(excel, index_col=0) #get raw data
+    df = pd.DataFrame(file) #get dataframe
+    data = np.array(df[0:nSample]) #jadiin array
+    
+    return data
+
+def convertDatatoDf(array: list[int]):
+    """
+    menerima masukan berupa numpy array 2D
+    dan keluaran berupa dataframe
+    
+    """
+    df = pd.DataFrame(array)
+    df.index = [f'Nilai ke-{i+1}' for i in range(len(df))]
+    
+    return df 
+
+def convertDatatoExcel(array: list[int], excel: str):
+    """
+    menerima masukan berupa numpy array 2D dan judul excel
+    dan keluaran berupa file excel
+    """
+    df = convertDatatoDf(array)
+    df.to_excel(f'{excel}.xlsx')
+    
+def fastFourierTransform(array: list[int], fs = 1/400, nSample = 1024):
+    """
+    menerima masukan berupa list array dan keluaran berupa 
+    hasil fft, magnituda dan fasa
+    
+    """
+    s_fft = np.fft.rfft(array)
+    s_mag = np.abs(s_fft)  # magnitude spectrum
+    s_phase = np.angle(s_fft, deg=True) #phase spectrum
+    s_freq = np.fft.rfftfreq(nSample)*fs
+    
+    return s_fft, s_mag, s_phase, s_freq
+
+def preprocessing(array: list[list], nSampleX, nSample=1024):
+    array[nSampleX:nSample] = 0
+    array = array - np.mean(array)
+    #array = signal.medfilt(array)
+    #array = signal.wiener(array)
+    #array = signal.detrend(array) #kurangi sama nilai rata-rata
+    array[nSampleX:nSample] = 0 #set remaining values to zero
+    array = array*np.hamming(nSample) #windowing using hamming
+    
+    return array
+
+# def rangeDopplerMatrix(array: list[int]):
+
+#     range_doppler_matrix = fftshift(fft2(array))
+    
+#     return range_doppler_matrix
+
+def spectrum(array: list[int], nSampleX):
+    """
+    menerima masukan berupa list array dan keluaran berupa 
+    Power Spectral Density (PSD) terhadap mean
+    
+    """
+    s_fft, s_mag, s_phase, s_freq= fastFourierTransform(array) #fft
+    sdB = 20*np.log10(s_mag/(nSampleX) + 0.001) #bikin ke skala logaritma
+    sdB = sdB*sdB/15 #meningkatkan kontras
+    
+    return sdB
+
+def getPeak(array: list[int], height=40, prominence=0.4, width=1):
+    peak, _ = signal.find_peaks(array, height=height, prominence=prominence, width=width)
+    return max(peak)
+
+"""
+if __name__ == '__main__':
+    data = readExcelData('dataradar_new.xlsx')
+    psd = powerSpectralDensity(data)
+    #df = convertDatatoExcel(psd)
+    
+    s_fft, s_mag, s_phase, s_freq = fastFourierTransform(data)
+    
+    for i in range(len(psd)):
+        x = sort_peak_from_spectrum(psd[i], s_freq)
+        
+        df = pd.DataFrame(x)
+        print(df) #yang tertinggi selalu default signal     
+"""
---
+++ main.py
@@ -0,0 +1,131 @@
+# importing libraries
+from PyQt5.QtWidgets import *
+from PyQt5 import QtCore, QtGui
+from PyQt5.QtGui import *
+from PyQt5.QtCore import *
+import pyqtgraph as pg
+import sys
+from getspectrum import preprocessing, spectrum, convertDatatoExcel, getPeak
+import pandas as pd
+import serial
+import numpy as np
+
+raw = serial.Serial(port='COM6', baudrate=20000000, timeout=1)
+nSample = 1024
+
+class Plot():
+    def __init__(self):
+        super().__init__()
+        
+        # Create the plot window
+        self.app = QApplication([])
+        self.windowplot = pg.GraphicsLayoutWidget()
+        self.windowplot.setFixedWidth(1000)
+
+        # Set up for raw data plot
+        self.rawdata = self.windowplot.addPlot(row=0, col=0, title='Raw Data Plot') # Add subplot to window
+        self.rawdata.setRange(xRange=[0, nSample]) # Set range to plot 
+        self.rawdata_plot = self.rawdata.plot(pen='y')
+        self.rawdata.setFixedWidth(500)
+        
+        #Set up for spectrum
+        self.spectrum = self.windowplot.addPlot(row=0, col=1, title='Spectrum Plot') # Add subplot to window
+        self.spectrum.setRange(xRange=[1, 512]) # Set range to plot 
+        self.spectrum_plot = self.spectrum.plot(pen='y')
+        self.spectrum.setFixedWidth(500)
+        
+        # Set up for max index
+        self.max_index = self.windowplot.addLabel(row=1, col=0, size='20pt', text='Max Index: -1') # Add subplot to window
+        
+        self.windowplot.show()
+        
+    def update_rawdata(self, data):
+        self.rawdata_plot.setData(data)
+    
+    def update_spectrum(self, data):
+        self.spectrum_plot.setData(data)
+        
+    def update_range(self, nSampleX):
+        self.rawdata.setRange(xRange=[0, nSampleX]) # Set range to plot
+        
+    def update_max_index(self, data):
+        index = getPeak(data)
+        self.max_index.setText(f'Max Index: {index}')
+        
+    def run(self):
+        self.app.instance().exec_()
+        
+    def stop(self):
+        self.app.exit()
+            
+if __name__ == '__main__':
+    plot = Plot()
+    
+    raw_data = []
+    spectrum_data = []
+    
+    def update():
+        dat1 = raw.read(nSample*2) # Read raw data from serial
+        dat2 = np.frombuffer(dat1, dtype='int16', offset=0) # Convert to int16
+        
+        # if len(dat2) == nSample:
+        #     s = np.array(dat2[0:nSample])
+        #     cekF0F = s[nSample - 1]
+        #     nSampleX = s[nSample - 2]
+        #     sr = s[nSample - 3]
+        #     prf = s[nSample - 4]
+        #     cekIns = s[nSample - 5]
+        # else:
+        #     cekF0F = 0
+
+        # if cekF0F == 3855:
+        #     s[nSampleX:nSample] = 0
+        #     s = s - (np.sum(s)) / nSampleX
+            
+        #     s[nSampleX:nSample] = 0
+        #     ss = s
+        #     print(ss)
+        #     s = s * np.hamming(nSample)
+        #     # S = 20*np.log10(abs(np.fft.rfft(s))/np.sqrt(nSampleX)+.001)
+        #     S = 20 * np.log10(abs(np.fft.rfft(s)) / (nSampleX) + 0.001)
+        #     S = S * S / 15
+        #     raw_data.append(ss)
+        #     spectrum_data.append(S)
+        #     # S = 2*(S-30)
+
+        #     plot.update_rawdata(ss)
+        #     plot.update_spectrum(S)
+        
+        s = np.array(dat2[0:nSample]) # Make np array
+        
+        # Read some variables
+        nSampleX = s[nSample-2]
+        #sf = s[nSample-3]
+        #prf = s[nSample-4]
+        plot.update_range(nSampleX)
+        s = preprocessing(s, nSampleX) # Data preprocessing
+        spect = spectrum(s, nSampleX) # Calculate PSD
+        
+        raw_data.append(s)
+        spectrum_data.append(spect)
+        
+        plot.update_rawdata(s)
+        plot.update_spectrum(spect)
+        plot.update_max_index(spect)
+
+    try:
+        while raw.is_open():
+            raw.flushInput()
+            raw.flushOutput()
+            timer = QtCore.QTimer()
+            timer.timeout.connect(update)
+            timer.start(1)
+            plot.run()
+    except KeyboardInterrupt: # masih ada masalah
+        plot.stop()
+        convertDatatoExcel(raw_data, 'rawdata')
+        convertDatatoExcel(spectrum_data, 'spectrum')
+        print("Data collection stopped.")
+        sys.exit()
+
+            
